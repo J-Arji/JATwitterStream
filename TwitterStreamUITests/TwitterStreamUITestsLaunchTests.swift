f7cd561d11771534b395ae1478b62f0129a82b2f
David Dawson <david.dawson@machine.ventures>
2022-04-06 17:06:10 +0430

    Initial Commit

---
+++ TwitterStreamUITests/TwitterStreamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TwitterStreamUITestsLaunchTests.swift
+//  TwitterStreamUITests
+//
+//  Created by javad Arji on 4/6/22.
+//
+
+import XCTest
+
+class TwitterStreamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
