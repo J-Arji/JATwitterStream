71dae2cc742147addffd504314ced8874497b55d
David Dawson <david.dawson@machine.ventures>
2022-04-11 20:05:09 +0430

    add navigation protocol

---
+++ TwitterStream/Protocol/Navigation.swift
@@ -0,0 +1,13 @@
+//
+//  Navigation.swift
+//  TwitterStream
+//
+//  Created by javad Arji on 4/11/22.
+//
+
+import UIKit
+
+protocol NavigationProtocol: AnyObject {
+    func push(_ vc: UIViewController)
+   
+}
